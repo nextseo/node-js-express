26383dcb6b1e14302aad58638120c0d5c9c06104
naiiwolf <naiiwolf00@gmail.com>
2022-02-28 18:43:58 +0700

    v 3

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const port =  3000;
 app.use(morgan('combined'));
 
 app.set('views','./src/views');
-app.set('view','ejs');
+app.set('view engine','ejs');
 
 app.get('/', (req,res,next)=> {
     res.render('index');
 })
+
 
 app.listen(port , ()=>{
     debug('SERVER ON PORT '+ port);
