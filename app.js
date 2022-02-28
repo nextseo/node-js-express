b372d1066fdfb25061bd360f15c019f2787d63c3
naiiwolf <naiiwolf00@gmail.com>
2022-02-28 19:13:44 +0700

    v4

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const debug = require('debug');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
-const port =  3000;
+const port =  4000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,'/public/')));
 
 app.set('views','./src/views');
 app.set('view engine','ejs');
